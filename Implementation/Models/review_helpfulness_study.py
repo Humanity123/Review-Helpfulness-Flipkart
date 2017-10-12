8ab880a0759bdae930a908fcf5cd7b77d74dbbf1
Humanity123 <yashagrwl.iitkgp@gmail.com>
2017-10-13 02:21:03 +0530

    Adding to review_helpfulness_study script

--- Implementation/Models/review_helpfulness_study.py
+++ Implementation/Models/review_helpfulness_study.py
@@ -1,14 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import os 
 
 sys.path.append("../Parsers")
 from rev_Parser import * 
 
-def get_helpfulness_vs_deviation(reviews, absolute=True):
+def get_helpfulness_vs_deviation(reviews, absolute=True, fname="helpfulness_deviation_graph.png"):
 	''' function to get plot of review helpfulness vs the devation from average rating'''
 
-	average_rating = np.mean([review['overall'] for review in reviews])
+	average_rating = round( np.mean([review['overall'] for review in reviews]) *2) / 2.0
 	deviation_helpfulness_dic = {}
 	for review in reviews:
 		deviation = review['overall'] - average_rating
@@ -20,27 +21,41 @@ def get_helpfulness_vs_deviation(reviews, absolute=True):
 			deviation_helpfulness_dic[deviation].append(review['helpful'])
 
 	x_values = deviation_helpfulness_dic.keys()
-	y_values = [np.mean(deviation_helpfulness_dic[x_value]) for x_value in x_values]
-	y_lower  = [y_values[index] - np.min(deviation_helpfulness_dic[x_value]) for index, x_value in enumerate(x_values)] 
-	y_upper  = [np.max(deviation_helpfulness_dic[x_value]) - y_values[index] for index, x_value in enumerate(x_values)] 
-	helpfulness_vs_deviation_plot = plt.errorbar(x_values, y_values, yerr=[y_lower, y_upper])
-	print x_values
-	print y_values
-	plt.show()
-	return helpfulness_vs_deviation_plot
+	y_values = [deviation_helpfulness_dic[x_value] for x_value in x_values]
+	
+	fig, ax = plt.subplots()
+	ax.set_title('Helpfulness vs Deviation from Average Star Rating')
+	ax.boxplot(y_values, labels=x_values, whis = 0, showfliers=False)
+	plt.savefig(fname, bbox_inches="tight")
+	
+	
 
 def get_variance(reviews):
 	''' returns the standard deviation of standard deviation of review's product rating rounded of to nearest 0.5 increment'''
 	return round( np.std([review['overall'] for review in reviews])*2 ) / 2.0
 
+
+def get_variance_product_dic(product_review_dic):
+	''' returns the dictionary with keys as variance and values as product ids'''
+	variance_product_dic = {}
+	for product, reviews in product_review_dic.iteritems():
+		variance = get_variance(reviews)
+		if variance not in variance_product_dic:
+			variance_product_dic[variance] = [product]
+		else:
+			variance_product_dic[variance].append(product)
+
+	return variance_product_dic
+
+
 def main():
-	reviews = get_review_data("/home/kushagra/Documents/BTP/my_work/crawled_data/Cell_Phones_and_Accessories_5.json")
+	reviews = get_review_data("")
 	target_prod = ""
 	for prod, rev_list in reviews.iteritems():
 		if len(rev_list) > 100:
 			target_prod = prod
 			break
-	get_helpfulness_vs_deviation(reviews[target_prod], True)
+	get_helpfulness_vs_deviation(reviews[target_prod], True, "Absolute_Deviation_Graph.png")
 if __name__=='__main__':
 	main()
 
