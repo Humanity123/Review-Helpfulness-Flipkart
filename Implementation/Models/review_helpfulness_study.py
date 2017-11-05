a5a09fe663704276ca22895ad2caeed432dc2588
Humanity123 <yashagrwl.iitkgp@gmail.com>
2017-11-05 10:59:40 +0530

    Adding Modified Files

--- Implementation/Models/review_helpfulness_study.py
+++ Implementation/Models/review_helpfulness_study.py
@@ -6,25 +6,27 @@ import os
 sys.path.append("../Parsers")
 from rev_Parser import * 
 
-def get_helpfulness_vs_deviation(reviews, absolute=True, fname="helpfulness_deviation_graph.png"):
-	''' function to get plot of review helpfulness vs the devation from average rating'''
+def get_helpfulness_vs_deviation(product_reviews, absolute=True, fname="helpfulness_deviation_graph.png", fig_name = ""):
+	''' function to get plot of review helpfulness vs the devation from average rating for a dictionary of producut reviews'''
 
-	average_rating = round( np.mean([review['overall'] for review in reviews]) *2) / 2.0
 	deviation_helpfulness_dic = {}
-	for review in reviews:
-		deviation = review['overall'] - average_rating
-		if absolute :
-			deviation = abs(deviation)
-		if deviation not in deviation_helpfulness_dic:
-			deviation_helpfulness_dic[deviation] = [review['helpful']]
-		else :
-			deviation_helpfulness_dic[deviation].append(review['helpful'])
+	for reviews in product_reviews.values():
+		average_rating = round( np.mean([review['overall'] for review in reviews]) *2) / 2.0
+		for review in reviews:
+			deviation = review['overall'] - average_rating
+			if absolute :
+				deviation = abs(deviation)
+			if deviation not in deviation_helpfulness_dic:
+				deviation_helpfulness_dic[deviation] = [review['helpful']]
+			else :
+				deviation_helpfulness_dic[deviation].append(review['helpful'])
 
 	x_values = deviation_helpfulness_dic.keys()
+	x_values.sort()
 	y_values = [deviation_helpfulness_dic[x_value] for x_value in x_values]
 	
 	fig, ax = plt.subplots()
-	ax.set_title('Helpfulness vs Deviation from Average Star Rating')
+	ax.set_title('Helpfulness vs Deviation from Average Star Rating: ' + fig_name)
 	ax.boxplot(y_values, labels=x_values, whis = 0, showfliers=False)
 	plt.savefig(fname, bbox_inches="tight")
 	
@@ -32,7 +34,7 @@ def get_helpfulness_vs_deviation(reviews, absolute=True, fname="helpfulness_devi
 
 def get_variance(reviews):
 	''' returns the standard deviation of standard deviation of review's product rating rounded of to nearest 0.5 increment'''
-	return round( np.std([review['overall'] for review in reviews])*2 ) / 2.0
+	return round( (np.std([review['overall'] for review in reviews])**2) *2 ) / 2.0
 
 
 def get_variance_product_dic(product_review_dic):
@@ -49,16 +51,25 @@ def get_variance_product_dic(product_review_dic):
 
 
 def main():
-	reviews = get_review_data("")
-	target_prod = ""
-	for prod, rev_list in reviews.iteritems():
-		if len(rev_list) > 100:
-			target_prod = prod
-			break
-	get_helpfulness_vs_deviation(reviews[target_prod], True, "Absolute_Deviation_Graph.png")
+	# reviews = get_review_data("/Users/yash/Documents/Acads/data/Flipkart/reviews_Electronics_5.json")
+	reviews = get_review_data("/Users/yash/Documents/Acads/data/Flipkart/Sports_and_Outdoors_5.json")
+	# reviews = get_review_data("/Users/yash/Documents/Acads/data/Flipkart/Cell_Phones_and_Accessories_5.json")	
+	get_helpfulness_vs_deviation(reviews, True, "Absolute_Deviation_Graph_Sports"+".png")
+	get_helpfulness_vs_deviation(reviews, False, "Signed_Deviation_Graph_Sports"+".png")
+	variance_product_dic = get_variance_product_dic(reviews)
+	for var in [0.0,0.5,1.0,1.5,2.0,2.5,3.0,3.5,4.0]:
+		try:
+			common_variance_products = variance_product_dic[var]
+		except:
+			continue
+		target_product_review_dic = {}
+		for product in variance_product_dic[var]:
+			target_product_review_dic[product] = reviews[product]
+
+		get_helpfulness_vs_deviation(target_product_review_dic, False, "Signed_Deviation_Graph_Sports"+str(var)+".png", str(var))
+
 if __name__=='__main__':
 	main()
-
 
 
 
--- Implementation/Parsers/rev_Parser.py
+++ Implementation/Parsers/rev_Parser.py
@@ -1,21 +1,26 @@
 import json
 from collections import defaultdict
 
-def get_review_data(file):
+def get_review_data(file_path):
 	reviews = []
-	for line in open("/home/kushagra/Documents/BTP/my_work/crawled_data/Cell_Phones_and_Accessories_5.json", 'r'):
+	for line in open(file_path, 'r'):
 		reviews.append(json.loads(line))
 	answer = defaultdict(lambda: list())
+	count = 0
 	for review in reviews:
 		votes = review['helpful']
-		if votes[1]==0:
-			continue
+		# if votes[1]==0:
+		# 	continue
+		if votes[1] < 10:
+			continue		
+		count += 1
 		review['helpful'] = votes[0]*1.0/votes[1]
-		answer[review['asin']].append(review)
+		answer[review['asin']].append({'helpful':review['helpful'], 'overall':review['overall']})
+	print count
 	return answer
 
 def main():
-	get_review_data("/home/kushagra/Documents/BTP/my_work/crawled_data/Cell_Phones_and_Accessories_5.json")
+	get_review_data("/Users/yash/Documents/Acads/data/Cell_Phones_and_Accessories_5.json")
 
 if __name__=='__main__':
 	main()
